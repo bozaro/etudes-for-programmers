c7e3fe35941e4780a9fbbc601f104908f0ecef8d
Artem V. Navrotskiy <bozaro@yandex.ru>
2013-03-05 20:47:53 +0400

    Перевел сборку на gradle

--- book.sh
+++
@@ -1,3 +0,0 @@
-fop -c config.xml -l ru -xml book.xml -xsl /usr/share/sgml/docbook/stylesheet/xsl/docbook-xsl/fo/docbook.xsl -pdf book.pdf \
-    -param body.font.family Serif \
-    -param title.font.family Serif
