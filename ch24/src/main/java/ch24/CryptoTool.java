7f2ebf5eaf55a9e89d00944246729da578b9d68a
Artem V. Navrotskiy <bozaro@yandex.ru>
2013-04-09 09:15:26 +0400

    Добавил генерацию таблицы с частотой символов.

--- ch24/src/main/java/ch24/CryptoTool.java
+++ ch24/src/main/java/ch24/CryptoTool.java
@@ -44,8 +44,74 @@ public class CryptoTool {
         }
 
         String text = IOUtils.toString(new FileInputStream(param.inFile), StandardCharsets.UTF_8.name());
-        String crypto = Vigenere.encrypt(param.base.toUpperCase(), param.crypt.toUpperCase(), param.keyword.toUpperCase(), text.toUpperCase());
-        System.out.println(crypto);
+        final String encrypt = Vigenere.encrypt(param.base.toUpperCase(), param.crypt.toUpperCase(), param.keyword.toUpperCase(), text.toUpperCase());
+        final String decrypt = Vigenere.idxToStr(param.base.toUpperCase(), Vigenere.strToIdx(param.base.toUpperCase(), text.toUpperCase()));
+
+        param.outDir.mkdirs();
+        log.info("Output directory: " + param.outDir.getAbsolutePath());
+        writeFile(param.outDir, "alphabet-crypt.txt", param.crypt);
+        writeFile(param.outDir, "keyword.txt", param.keyword);
+        writeFile(param.outDir, "encrypted.txt", Vigenere.format(encrypt, param.block, param.line));
+        writeFile(param.outDir, "space-keyword.txt", Vigenere.formatSpaced(param.keyword, param.base.toUpperCase(), text.toUpperCase()));
+        writeFile(param.outDir, "space-encrypted.txt", Vigenere.formatSpaced(encrypt, param.base.toUpperCase(), text.toUpperCase()));
+        writeFile(param.outDir, "space-decrypted.txt", Vigenere.formatSpaced(decrypt.toUpperCase(), param.base.toUpperCase(), text.toUpperCase()));
+        writeFile(param.outDir, "vigenere.txt", Vigenere.square(param.base.toUpperCase(), param.crypt.toUpperCase()));
+        // Слова по длине.
+        List<String> words = Vigenere.findWords(encrypt, 4);
+        for (int i = 0; i < Math.min(words.size(), param.words); ++i) {
+            writeFile(param.outDir, "word-len-" + i + ".txt", words.get(i));
+        }
+        // Слова по частоте.
+        Collections.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int cmp = Integer.compare(encrypt.replaceAll(s2, "").length() * s1.length(), encrypt.replaceAll(s1, "").length() * s2.length());
+                if (cmp != 0) {
+                    return cmp;
+                }
+                return Integer.compare(s2.length(), s1.length());
+            }
+        });
+        for (int i = 0; i < Math.min(words.size(), param.words); ++i) {
+            writeFile(param.outDir, "word-cnt-" + i + ".txt", words.get(i));
+        }
+        // Слова случайные.
+        List<String> dictWords = findDictWords(encrypt, 2);
+        Collections.sort(dictWords, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return Integer.compare(s2.length(), s1.length());
+            }
+        });
+        for (int i = 0; i < Math.min(dictWords.size(), param.words); ++i) {
+            writeFile(param.outDir, "word-dict-" + i + ".txt", dictWords.get(i));
+        }
+    }
+
+    private static void writeFile(File basePath, String fileName, String content) throws IOException {
+        try (OutputStream stream = new FileOutputStream(new File(basePath, fileName))) {
+            stream.write(content.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    public static List<String> findDictWords(String text, int minLength) throws IOException {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(CryptoTool.class.getResourceAsStream("/ch24/word_ru.txt"), StandardCharsets.UTF_8))) {
+            while (true) {
+                String line = reader.readLine();
+                if (line == null) {
+                    break;
+                }
+                String word = line.trim().toUpperCase();
+                if (word.length() < minLength) {
+                    continue;
+                }
+                if (text.contains(word)) {
+                    result.add(word);
+                }
+            }
+        }
+        return result;
     }
 
     public static class Args {
@@ -55,7 +121,7 @@ public class CryptoTool {
 
         @Option(name = "-o", usage = "output to this directory", metaVar = "OUTPUT")
         @NotNull
-        private File outDir = new File("generated");
+        private File outDir = new File("build/generated-test");
 
         @Option(name = "-b", usage = "base alphabet")
         private String base = null;
@@ -65,5 +131,14 @@ public class CryptoTool {
 
         @Option(name = "-a", usage = "key alphabet", required = true)
         private String crypt;
+
+        @Option(name = "--block", usage = "character block size")
+        private int block = 5;
+
+        @Option(name = "--line", usage = "maximum line size")
+        private int line = 70;
+
+        @Option(name = "--words", usage = "words count")
+        private int words = 5;
     }
 }
---
+++ ch24/src/main/java/ch24/DictTool.java
@@ -0,0 +1,89 @@
+package ch24;
+
+import ch24.decrypt.Alphabet;
+import org.jetbrains.annotations.NotNull;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.ExampleMode;
+import org.kohsuke.args4j.Option;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+/**
+ * Генерация данных об алфавите.
+ *
+ * @author Artem V. Navrotskiy
+ */
+public class DictTool {
+    private static final Logger log = Logger.getLogger(DictTool.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        Args param = new Args();
+        CmdLineParser parser = new CmdLineParser(param);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            parser.setUsageWidth(80);
+            System.err.println(e.getMessage());
+            System.err.println("Usage: " + DictTool.class.getName() + parser.printExample(ExampleMode.ALL));
+            parser.printUsage(System.err);
+            System.err.println();
+            return;
+        }
+        param.outDir.mkdirs();
+        log.info("Output directory: " + param.outDir.getAbsolutePath());
+        try (InputStream stream = DictTool.class.getResourceAsStream("/ch24/alphabet_ru.txt")) {
+            Alphabet alphabet = Alphabet.load(stream);
+            double rate = 0;
+            for (double freq : alphabet.getFrequency()) {
+                rate += (freq * freq);
+            }
+
+            Locale locale = new Locale(param.locale);
+            log.info("Locale: " + locale.toString());
+            DecimalFormat format = new DecimalFormat(param.format, DecimalFormatSymbols.getInstance(locale));
+            writeFile(param.outDir, "freq-min.txt", format.format(rate - param.delta));
+            writeFile(param.outDir, "freq-norm.txt", format.format(rate));
+            writeFile(param.outDir, "freq-max.txt", format.format(rate + param.delta));
+
+            StringBuilder table = new StringBuilder();
+            table.append("<tbody xmlns=\"http://docbook.org/ns/docbook\">\n");
+            for (int idx : alphabet.getSorted()) {
+                table.append("<row><entry>")
+                        .append(alphabet.getAlphabet().charAt(idx))
+                        .append("</entry><entry>")
+                        .append(format.format(alphabet.getFrequency()[idx]))
+                        .append("</entry></row>\n");
+            }
+            table.append("</tbody>\n");
+            writeFile(param.outDir, "table.xml", table.toString());
+        }
+    }
+
+    private static void writeFile(File basePath, String fileName, String content) throws IOException {
+        try (OutputStream stream = new FileOutputStream(new File(basePath, fileName))) {
+            stream.write(content.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    public static class Args {
+        @Option(name = "-o", usage = "output to this directory", metaVar = "OUTPUT")
+        @NotNull
+        private File outDir = new File("build/generated-test");
+
+        @Option(name = "--delta", usage = "freq delta")
+        private double delta = 0.01;
+
+        @Option(name = "--locale", usage = "number locale")
+        private String locale = "ru_RU";
+
+        @Option(name = "--format", usage = "number format")
+        private String format = "0.000";
+    }
+
+}
